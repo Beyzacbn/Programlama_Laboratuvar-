71cfe38cd26a29259834c24ed2b7f0abc6b2ef1b
BeyzaCoban <beyzacbn@gmail.com>
2019-03-18 11:23:45 +0300

    asal_kontrol

---
+++ asal.py
@@ -0,0 +1,18 @@
+def asal(n):
+
+    control = 0
+
+    for i in range(2, int(n)):
+        if (int(n) % i == 0):
+            n=n/i
+            control += 1
+            print(i)
+           
+
+    if (control != 0):
+        print("Sayı Asal Değil")
+    else:
+        print("Sayı Asal")
+
+
+print(asal(13))
