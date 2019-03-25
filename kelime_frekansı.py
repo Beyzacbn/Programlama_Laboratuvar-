333b66c179ccf95cec0068890899ff6f7ca0f76f
BeyzaCoban <beyzacbn@gmail.com>
2019-03-25 20:18:38 +0300

    okunan dosyanın içindeki kelimelerin frekansını bulduk

---
+++ kelime_frekansı.py
@@ -0,0 +1,51 @@
+class myClass():
+    def __init__(self,myfile=" "):
+	##htttps://www.gutenberg.org/files/1342/1342-0.txt
+	f= open("test_1.txt","r")
+	myContent=f.read()
+	mySentences= myContent.split(".")
+	self.myWords=[]
+	for sentence in mySentences:
+	    Words_In_the_sentence=sentence.split(" ")
+	    for word_1 in Words_In_the_sentence:
+		self.myWords.append(word_1)
+	self.my_frequency_1()
+	self.write_frequency_1()
+	self.my_frequency_2()
+	self.write_frequency_2()
+
+
+    def my_frequency_1(self):
+	self.frequency_list_1={}
+	for word in seflf.myWords:
+	    if( word in self.frequency_list_1):
+		self.frequency_list_1[word]+=1
+	    else:
+		self.frequency_list_1[word]=1
+
+    def write_frequency_1(self):
+	for word_1 in self.frequency_list_1:
+	    print(word_1+" "+str(self.frequency_list_1[word_1]))
+
+
+    def my_frequency_2(self):
+	self.frequency_list_2={}
+	for i in range (len(self.myWords)-1):
+	    w_1,w_2= self.myWords[i],self.myWords[i+1]
+
+	    if( (w_1,w_2) in self.frequency_list_2):
+		self.frequency_list_2[(w_1,w_2)]+=1
+	    else:
+		self.frequency_list_2[(w_1,w_2)]=1
+
+    def write_frequency_2(self):
+	for w_1 in self.frequency_list_2:
+	    print(str(w_1)+" "+str(self.frequency_list_2[w_1]))
+
+    
+    my_class_1=myClass()
+    print(len(my_class_1.frequency_list_1))
+    print(my_class_1.frequency_list_1['all'])
+
+	    
+	    
